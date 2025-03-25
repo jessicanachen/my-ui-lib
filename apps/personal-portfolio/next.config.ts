5858901af7f11fd56fead40bd942505622e9637b
jessicanachen <jessicanachen@gmail.com>
2025-03-25 16:18:49 -0400

    Created personal-portfolio and fixed linting errors

---
+++ apps/personal-portfolio/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  transpilePackages: ["ui"], 
+  reactStrictMode: true,
+};
+
+export default nextConfig;
---
+++ apps/personal-portfolio/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Card } from "@repo/ui/card";
+
+
+export default function Home() {
+  return (
+    <Card href="/" key="1" title="sldkfjslkd">
+      skldjflskdjflksdjflksdj
+    </Card>
+  );
+}
